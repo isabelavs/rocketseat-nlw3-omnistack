4d7082013c9300ad487ed7d5ac0422b11cc6e6f4
Isabela Santos <isabela.vieir@hotmail.com>
2020-10-18 23:29:35 -0300

    ip

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -5,7 +5,7 @@ export default {
     return {
       id: image.id,
       // melhor me variaveis ambiente
-      url: `http://localhost:3333/uploads/${image.path}`,
+      url: `http://192.168.100.84:3333/uploads/${image.path}`,
     };
   },
 
